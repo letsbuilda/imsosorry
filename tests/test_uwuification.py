16e4c95e88b78ecf91a3db5c3973cb4c98067fbc
Bradley Reynolds <bradley.reynolds@darbia.dev>
2023-01-25 20:03:49 -0600

    uwu: add test for emoji replace
    Signed-off-by: Bradley Reynolds <bradley.reynolds@darbia.dev>

--- tests/test_uwuification.py
+++ tests/test_uwuification.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 import pytest
 
-from imsosorry.uwuification import word_replace
+from imsosorry.uwuification import EMOJIS, emoji, word_replace
 
 
 @pytest.mark.parametrize(
@@ -16,3 +16,15 @@ from imsosorry.uwuification import word_replace
 )
 def test_word_replace(in_text: str, out_text: str) -> None:
     assert word_replace(in_text) == out_text
+
+
+@pytest.mark.parametrize(
+    "strength,has_emoji",
+    [
+        (0.0, False),
+        (1.0, True),
+    ],
+)
+def test_emoji(strength: float, has_emoji: bool) -> None:
+    output = emoji("I love dogs!", strength)
+    assert any(emoji_ in output for emoji_ in EMOJIS) == has_emoji
