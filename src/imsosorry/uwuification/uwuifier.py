324c61b6528e73af0e8e042d3dfab2423677013b
Bradley Reynolds <bradley.reynolds@darbia.dev>
2024-03-24 06:12:42 +0000

    Move uwuifier into submodule
    Signed-off-by: GitHub <noreply@github.com>

--- src/imsosorry/uwuification/uwuifier.py
+++ src/imsosorry/uwuification/uwuifier.py
@@ -3,81 +3,24 @@
 from __future__ import annotations
 
 import random
-import re
 from copy import copy
 from functools import partial
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
+    import re
     from collections.abc import Callable
 
-WORD_REPLACE = {
-    "small": "smol",
-    "cute": "kawaii~",
-    "fluff": "floof",
-    "love": "luv",
-    "stupid": "baka",
-    "idiot": "baka",
-    "what": "nani",
-    "meow": "nya~",
-    "roar": "rawrr~",
-}
-"""A dict to match certain words for replacement words"""
-
-EMOJIS = [
-    "rawr x3",
-    "OwO",
-    "UwU",
-    "o.O",
-    "-.-",
-    ">w<",
-    "(⑅˘꒳˘)",
-    "(ꈍᴗꈍ)",
-    "(˘ω˘)",
-    "(U ᵕ U❁)",
-    "σωσ",
-    "òωó",
-    "(///ˬ///✿)",
-    "(U ﹏ U)",  # noqa: RUF001 - literally the point...
-    "( ͡o ω ͡o )",
-    "ʘwʘ",
-    ":3",
-    ":3",  # important enough to have twice
-    "XD",
-    "nyaa~~",
-    "mya",
-    ">_<",
-    "😳",
-    "🥺",
-    "😳😳😳",
-    "rawr",
-    "^^",
-    "^^;;",
-    "(ˆ ﻌ ˆ)♡",  # noqa: RUF001 - literally the point...
-    "^•ﻌ•^",
-    "/(^•ω•^)",
-    "(✿oωo)",
-]
-"""A list of emojis/emoticons to add."""
-
-REGEX_WORD_REPLACE = re.compile(r"(?<!w)[lr](?!w)")
-"""A regex that to detect certain characters to change to "w"s."""
-
-REGEX_PUNCTUATION = re.compile(r"[.!?\r\n\t]")
-"""A regex to detect certain punctuation characters to emotify /(^•ω•^)"""
-
-REGEX_TILDE = re.compile(r"(?![^ ])(?<!\B)")
-"""A regex to find places to add tildes (~) to."""
-
-REGEX_STUTTER = re.compile(r"(\s)([a-zA-Z])")
-"""A regex to find words to stutter."""
-SUBSTITUTE_STUTTER = r"\g<1>\g<2>-\g<2>"
-"""A regex to add st-stuttering to strings."""
-
-REGEX_NYA = re.compile(r"n([aeou][^aeiou])")
-"""A regex to detect words with an n before a vowel to nyaify."""
-SUBSTITUTE_NYA = r"ny\1"
-"""A regex to to nyaify words."""
+from .constants import (
+    EMOJIS,
+    REGEX_NYA,
+    REGEX_PUNCTUATION,
+    REGEX_STUTTER,
+    REGEX_TILDE,
+    REGEX_WORD_REPLACE,
+    SUBSTITUTE_NYA,
+    WORD_REPLACE,
+)
 
 
 def word_replace(text: str) -> str:
@@ -143,7 +86,7 @@ def uwuify(
         word_replace,
         partial(re_sub, match_pattern=REGEX_NYA, replace_pattern=SUBSTITUTE_NYA),
         partial(re_sub, match_pattern=REGEX_WORD_REPLACE, replace_pattern="w"),
-        partial(re_sub_maybe, pattern=REGEX_TILDE, text_getter=stutter_string, strength=stutter_strength),
+        partial(re_sub_maybe, pattern=REGEX_STUTTER, text_getter=stutter_string, strength=stutter_strength),
         partial(re_sub_maybe, pattern=REGEX_PUNCTUATION, text_getter=emoji_string, strength=emoji_strength),
         partial(re_sub_maybe, pattern=REGEX_TILDE, text_getter=lambda _text: "~", strength=tilde_strength),
     ]
