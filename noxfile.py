287cb8f6bc2504ef8b833bf005b8d0d3272b9477
Bradley Reynolds <bradley.reynolds@darbia.dev>
2025-02-14 21:32:02 -0600

    Modernize repo (#101)
    * Remove PowerShell from DevContainer config
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * Purge CodeFlash
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * Moderize publishing workflow
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * Update dependencies
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * Switch from PowerShell to Nox
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * Modernize build system table
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * Remove unused task
    
    Signed-off-by: GitHub <noreply@github.com>
    
    * Bump pre-commit hooks
    
    Signed-off-by: GitHub <noreply@github.com>
    
    ---------
    
    Signed-off-by: GitHub <noreply@github.com>

---
+++ noxfile.py
@@ -0,0 +1,52 @@
+"""Noxfile."""
+
+import shutil
+from pathlib import Path
+
+import nox
+
+nox.options.default_venv_backend = "none"
+nox.options.sessions = ["lints"]
+
+
+CLEANABLE_TARGETS = [
+    "./dist",
+    "./build",
+    "./.nox",
+    "./.coverage",
+    "./.coverage.*",
+    "./coverage.json",
+    "./**/.mypy_cache",
+    "./**/.pytest_cache",
+    "./**/__pycache__",
+    "./**/*.pyc",
+    "./**/*.pyo",
+]
+
+
+@nox.session
+def tests(session: nox.Session) -> None:
+    """Run tests."""
+    session.run("pytest")
+
+
+@nox.session
+def lints(session: nox.Session) -> None:
+    """Run lints."""
+    session.run("pre-commit", "run", "--all-files")
+    session.run("ruff", "check", "--fix", ".")
+    session.run("ruff", "format", ".")
+    session.run("mypy", "--strict", "src/")
+
+
+@nox.session
+def clean(_: nox.Session) -> None:
+    """Clean cache, .pyc, .pyo, and test/build artifact files from project."""
+    count = 0
+    for searchpath in CLEANABLE_TARGETS:
+        for filepath in Path().glob(searchpath):
+            if filepath.is_dir():
+                shutil.rmtree(filepath)
+            else:
+                filepath.unlink()
+            count += 1
